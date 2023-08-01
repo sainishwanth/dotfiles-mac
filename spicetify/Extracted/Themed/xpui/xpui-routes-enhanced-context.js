9fcc6df19e459881779f6256fc92390ea4f2d2e9
Sai Nishwanth <sainishwanthraj@gmail.com>
2023-08-01 13:59:42 +0530

    Emacs

---
+++ spicetify/Extracted/Themed/xpui/xpui-routes-enhanced-context.js
@@ -0,0 +1,1 @@
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[7405,682],{7808:(e,l,s)=>{s.d(l,{s:()=>f.s});var f=s(82930)}}]);
---
+++ spicetify/Extracted/Themed/xpui/xpui-routes-lyrics.js
@@ -0,0 +1,1 @@
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[6025],{28352:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n.Z});var n=f(65489)}}]);
